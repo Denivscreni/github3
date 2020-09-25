2215bc79cf98593354d2a9b7d54bf9e763ffe7b9
admin <admin@DESKTOP-95F7QSE>
2020-09-25 08:55:43 +0300

    2 Commit

--- Form2.cs
+++ Form2.cs
@@ -10,18 +10,11 @@ using System.Windows.Forms;
 
 namespace github
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form2 f2 = new Form2();
-            this.Hide();
-            f2.Show();
         }
     }
 }
