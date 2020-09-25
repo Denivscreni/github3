2dbd23367f664757fdbeb2f32422d0f6a4386dcd
admin <admin@DESKTOP-95F7QSE>
2020-09-25 09:23:58 +0300

    Corda

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(414, 174);
+            this.button2.Location = new System.Drawing.Point(317, 106);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
